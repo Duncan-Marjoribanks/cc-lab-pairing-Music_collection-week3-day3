1b9daa6e623b1a40223127af60ac3599d6425f4f
Juan <juanmataruiz84@gmail.com>
2018-04-25 15:40:29 +0100

    def save album passes

--- db/console.rb
+++ db/console.rb
@@ -8,8 +8,12 @@ artist1 = Artist.new({"name" => "Adele"})
 artist1.save()
 
 
-
-
+album1 = Album.new({
+  "title" => "Hello",
+  "genre" => "Romantic",
+  "artist_id" => artist1.id
+  })
+album1.save()
 
 
 binding.pry
--- models/album.rb
+++ models/album.rb
@@ -14,6 +14,13 @@ class Album
     @artist_id = options["artist_id"].to_i
   end
 
+  def save()
+    sql = "INSERT INTO albums (title, genre, artist_id) VALUES ($1, $2, $3) RETURNING id"
+    values = [@title, @genre, @artist_id]
+    result = SqlRunner.run(sql, values)
+    @id = result.first["id"].to_i
+  end
+
 
 
 
