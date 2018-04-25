722d26018e571a198b245ba1dd3d981c6b94dd0e
Juan <juanmataruiz84@gmail.com>
2018-04-25 15:09:51 +0100

    create album and artist class

---
+++ models/album.rb
@@ -0,0 +1,21 @@
+require('pg')
+require_relative('../db/sql_runner')
+require_relative('artist')
+
+class Album
+
+  attr_accessor :title :genre :artist_id
+  attr_reader :id
+
+  def initialize(options)
+    @title = options["title"]
+    @genre = options["genre"]
+    @id = options["id"].to_i if options["id"]
+    @artist_id = options["artist_id"].to_i
+  end
+
+
+
+
+
+end
